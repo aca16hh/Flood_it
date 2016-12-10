3a35a3161854d7deed8ce438f0af0fbd6052cffd
Harry Hirst <hhirst1@sheffield.ac.uk>
2016-12-10 17:50:30 +0000

    I have created a menu screen

--- Assignment.rb
+++ Assignment.rb
@@ -6,7 +6,7 @@
 require 'console_splash'
 
 splash = ConsoleSplash.new(15,50)
-splash.write_header("Flood It", "Harry Hirst", "0.02")
+splash.write_header("Flood It", "Harry Hirst", "0.03")
 splash.write_center(-5, "Press Enter to Continue")
 splash.write_horizontal_pattern("*")
 splash.write_vertical_pattern("*")
@@ -16,25 +16,45 @@ puts""
 gets.chomp
 
 puts "WELL DONE, you pressed enter"
-def menu
+def menu(height, width)
   puts "MAIN MENU"
   puts "s = Start game"
   puts "c = change size"
   puts "q = quit"
+  puts "High  Score: unavailable"
 
   print "Please Enter Your Choice: "
   input = gets.chomp.downcase
   if input=="s"
-    menu
+    flood(height, width)
   elsif input=="c"
-    menu
+    print "What is the new height (currently #{height})? "
+    height = gets.chomp
+    print "What is the new width (currently #{width})? "
+    width = gets.chomp
+    menu(height, width)
   elsif input=="q"
     puts "Thank you for playing!"
   else
     puts "that's not a command"
-    menu
+    menu(height, width)
+  end
+end
+
+def flood(height, width)
+  board = Array.new[height, width]
+  i=0
+  j=0
+  while(i<height)
+    while(j<width)
+      board[i, j]=rand(0...5)
+      print(board[i, j]+" ")
+      j=j+1
+    end
+    puts""
+    i=i+1
   end
 end
 
 
-menu
+menu(9, 14)
