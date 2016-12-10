a5462c33f60cd1a4711f7907f561350307d49f45
Harry Hirst <hhirst1@sheffield.ac.uk>
2016-12-10 17:03:18 +0000

    I have now created a menu for my game

--- Assignment.rb
+++ Assignment.rb
@@ -4,8 +4,9 @@
 # Time: 04:36 PM
 # To change this template use Tools | Templates.
 require 'console_splash'
+
 splash = ConsoleSplash.new(15,50)
-splash.write_header("Flood It", "Harry Hirst", "0.01")
+splash.write_header("Flood It", "Harry Hirst", "0.02")
 splash.write_center(-5, "Press Enter to Continue")
 splash.write_horizontal_pattern("*")
 splash.write_vertical_pattern("*")
@@ -15,4 +16,25 @@ puts""
 gets.chomp
 
 puts "WELL DONE, you pressed enter"
-print "just work"
+def menu
+  puts "MAIN MENU"
+  puts "s = Start game"
+  puts "c = change size"
+  puts "q = quit"
+
+  print "Please Enter Your Choice: "
+  input = gets.chomp.downcase
+  if input=="s"
+    menu
+  elsif input=="c"
+    menu
+  elsif input=="q"
+    puts "Thank you for playing!"
+  else
+    puts "that's not a command"
+    menu
+  end
+end
+
+
+menu
