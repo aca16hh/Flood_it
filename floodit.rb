1a883ec0c8cd47c70c0a7e39048d5a2f36c72479
Harry Hirst <hhirst1@sheffield.ac.uk>
2016-12-14 05:23:52 +0000

    finished program including bug fixes

--- floodit.rb
+++ floodit.rb
@@ -6,28 +6,33 @@
 require 'console_splash'
 require 'colorize'
 
+#Here I initialize and apply the splash scrren upon start up
 splash = ConsoleSplash.new(15,50)
-splash.write_header("Flood It", "Harry Hirst", "0.03")
+splash.write_header("Flood It", "Harry Hirst", "0.99")
 splash.write_center(-5, "Press Enter to Continue")
 splash.write_horizontal_pattern("*")
 splash.write_vertical_pattern("*")
 splash.splash
-high = 0
 
+#This places a line to make it look neater then when the user presses enter, the menu method is activated
 puts""
 gets.chomp
-
+#I tried to create a loop to exit when the quit method is called
+continue=1
+until (!continue) do
 def menu(height, width, high)
-  puts "MAIN MENU"
+  #I list the options for the user's benefit
+  puts "main menu"
   puts "s = Start game"
   puts "c = change size"
   puts "q = quit"
-  if high==0
+  #This keeps track of the high score unless it is the first game in which case it displays a different message
+  if high==999
     puts "No games played yet."
-  elsif high>0
+  elsif high<999
     puts "Best game: #{high} turns"
   end
-
+  #here, the user can enter the letter corespondin to which option they want to select
   print "Please Enter Your Choice: "
   input = gets.chomp.downcase
   if input=="s"
@@ -40,21 +45,23 @@ def menu(height, width, high)
     width = gets.chomp.to_i
     menu(height, width, high)
   elsif input=="q"
-    puts "Thank you for playing!"
-    nil
+    quit(0, 0)
+  #this else clause helps catch if the user mis-types a command and restarts the menu
   else
     puts "that's not a command"
     menu(height, width, high)
   end
 end
 
-
+#this activates when the user starts a new game and draws out a canvas of random dots
 def get_board(height, width, high)
+  #I initialize my arrays
   board = Array.new(height){Array.new(width, 0)}
   group = Array.new(height){Array.new(width, 0)}
   colors = [":red", ":green", ":blue", ":cyan", ":magenta", ":yellow"]
   i=0
   j=0
+  #I pick a random colour and assign it to the array while I draw it
   until(i>=height) do
     while(j<width)
       random=colors[rand(0...6)]
@@ -77,19 +84,21 @@ def get_board(height, width, high)
         print("  ".colorize(:background => :magenta))
         board[i][j]=":magenta"
       end
+      #moves the loop to the next iteration
       j=j+1
     end
     puts""
     j=0
     i=i+1
   end
+  #calls the next method
   update(height, width, high, 0, board, group)
 end
 
-def draw_board(height, width, high, turns, complete, board, group)
+def draw_board(height, width, high, turns, board, group)
   i=0
   j=0
-  puts"devil child"
+  #same as get_board but doesn't assign to the arrays
   while(i<height)
     while(j<width)
       if(board[i][j]==":green")
@@ -111,16 +120,27 @@ def draw_board(height, width, high, turns, complete, board, group)
     j=0
     i=i+1
   end
-  play(height, width, high, turns, complete, board, group)
+  play(height, width, high, turns, board, group)
 end
 
-def play(height, width, high, turns, complete, board, group)
+def play(height, width, high, turns, board, group)
   i=0
   j=0
-  percent = (complete/(height*width))*100
+  completion=0
+  #adds to completion allowing me to calculate percentage later
+  while(i<height)
+    while(j<width)
+      completion=completion+(group[i][j].to_i)
+      j=j+1
+    end
+    j=0
+    i=i+1
+  end
+  #prints relevant data to gameplay
   puts "number of turns: #{turns}"
-  puts "Current completion: #{percent}%"
+  puts "Current completion: #{(completion*100)/(height*width)}%"
   print "Choose a colour: "
+  #reads user input and either quits or changes the top left block to their colour
   input=gets.chomp
   if input=="q"
     menu(height, width, high)
@@ -144,7 +164,7 @@ def play(height, width, high, turns, complete, board, group)
     board[0][0] =":magenta"
   else
     puts "That's not a valid option, please try again"
-    play(height, width, high, turns, complete, board, group)
+    play(height, width, high, turns, board, group)
   end
   update(height, width, high, turns, board, group)
 end
@@ -154,10 +174,17 @@ def update(height, width, high, turns, board, group)
   j=0
   group[0][0]=1
   base=board[0][0]
+  #keeps the user from going on in the game forever
+  if turns>=999
+    puts "YOU LOSE"
+    menu(height, width, high)
+  end
+  #complicated method which effectively checks all the blocks adjacent to those which are in the selected group to see if they are the same colour and if they are, it adds them to the group
   while(i<height)
     while(j<width)
       if(group[i][j]==1)
         board[i][j]=base
+        #these allow me to check for multiple new blocks in a line together
         iPlus=i+1
         jPlus=j+1
         iMinus=i-1
@@ -175,7 +202,7 @@ def update(height, width, high, turns, board, group)
           end
         end
         if iMinus>=1
-          until(board[iMinus][j]!=base) do
+          until(board[iMinus][j]!=base||iMinus==0) do
             if iMinus>=1
               group[iMinus][j]=1
               board[iMinus][j]=base
@@ -184,7 +211,7 @@ def update(height, width, high, turns, board, group)
           end
         end
         if jMinus>=1
-          if(board[i][jMinus]==base)
+          until(board[i][jMinus]!=base||jMinus==0) do
             if jMinus>=1
               group[i][jMinus]=1
               board[i][jMinus]=base
@@ -201,23 +228,24 @@ def update(height, width, high, turns, board, group)
   check(height, width, high, turns, board, group)
 end
         
-
+#this method checks if the array is all the same colour. If it is, the user wins
 def check(height, width, high, turns, board, group)
   i=0
   j=0
-  complete=0
+  puts"check"
   base=board[0][0]
   while(i<height)
     while(j<width)
       if(base!=board[i][j])
         if turns>0
-          draw_board(height, width, high, turns, complete, board, group)
+          draw_board(height, width, high, turns, board, group)
         elsif turns==0
-          play(height, width, high, turns, complete, board, group)
+          play(height, width, high, turns, board, group)
+        elsif turns<0
+          quit(0,0)
         end
       end
       j=j+1
-      complete=complete+1
     end
     j=0
     i=i+1
@@ -229,5 +257,17 @@ def check(height, width, high, turns, board, group)
   end
   menu(height, width, high)
 end
+
+#method to quit when the user presses q in the main menu
+def quit(height, width)
+  turns=-1
+  puts "Thank you for playing!"
+  continue=0
+  #an intentional error as I couldn't find another way to break the infinite loop
+  menu()
+end
     
-menu(9, 14, 0)
+#calls the start of the program
+menu(9, 14, 999)
+  
+end
